285fbc6e8de984851e01ca70d83a504bc69af841
rubenaguilardev <ragui56@wgu.edu>
2025-07-22 15:24:39 -0700

    create header, add logo and date. add roboto font to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import logo from '../assets/logo.png'
+import Navbar from './Navbar'
 
 const Header = () => {
 
     const date = new Date().toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})
 
     return (
-        <header className="w-full z-50 transition-all duration-300 border border-dashed">
-            <div className="container px-[52px] py-[38px] flex items-center justify-between">
-                <div className='flex items-center gap-7'>
-                    <img src={logo} alt="logo"/>
-                    <h1 className='text-[2rem] font-medium'>Surgery Status</h1>
+        <header className="w-full transition-all duration-300 border border-dashed">
+            <div className="container flex items-center justify-between px-[52px] py-[38px]">
+                <div className='flex items-center gap-8'>
+                    <a href="#">
+                        <div className='flex gap-7 items-center'>
+                            <img src={logo} alt="logo"/>
+                            <h1 className='text-[2rem] font-medium'>Surgery Status</h1>
+                        </div>
+                    </a>
                     <span className='ml-1 mt-[.6rem] text-[#4F378A] font-medium'>{date}</span>
                 </div>
+                <Navbar/>
             </div>
         </header>
     )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return (
+        <div>Hi</div>
+    )
+}
+
+export default Navbar
