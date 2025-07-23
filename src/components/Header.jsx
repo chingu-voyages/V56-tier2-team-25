40169d7ac7554969a86536ab1a5e0acf7c78c41b
rubenaguilardev <ragui56@wgu.edu>
2025-07-23 16:08:13 -0700

    add nav, links, button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
 
     return (
         <header className="w-full transition-all duration-300 border border-dashed">
-            <div className="container flex items-center justify-between px-[52px] py-[38px]">
+            <div className="container mx-auto flex items-center justify-between py-[38px]">
                 <div className='flex items-center gap-8'>
                     <a href="#">
                         <div className='flex gap-7 items-center'>
                             <img src={logo} alt="logo"/>
-                            <h1 className='text-[2rem] font-medium'>Surgery Status</h1>
+                            <h1 className='text-[2rem] font-medium text-shadow-md'>Surgery Status</h1>
                         </div>
                     </a>
                     <span className='ml-1 mt-[.6rem] text-[#4F378A] font-medium'>{date}</span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
 const Navbar = () => {
     return (
-        <div>Hi</div>
+        <nav className="flex items-center space-x-17 text-[#5D5A88] nav-links text-[1.125rem]">
+            <a href="#">Home</a>
+            <a href="#">Patient</a>
+            <a href="#">Help</a>
+            <a href="#" >
+                <button 
+                    className="text-white bg-[#4F3788] rounded-[30px] px-[24px] py-[18px] border-2 border-[#D4D2E3]
+                    shadow-xl/30 cursor-pointer">Log In
+                </button>
+            </a>
+        </nav>
+        
     )
 }
 
