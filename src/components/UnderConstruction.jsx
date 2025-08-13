72e603bb3b55c1e52df6289b39deb55a6f6837a6
rubenaguilardev <ragui56@wgu.edu>
2025-08-13 16:05:45 -0700

    update staff landing page styles, connect buttons to construction page

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,14 +1,24 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import construction from '../assets/construction.png'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+
 
 const UnderConstruction = () => {
+
+    const navigate = useNavigate()
+
     return (
         <>
             <div className="w-full min-h-screen flex flex-col">
                 <Header />
                 <section className="flex flex-1 flex-col items-center pt-11 md:pt-0 px-3 md:justify-center bg-[#F5F3EA]">
-                    <h2 className="text-[#4F4F4F] text-2xl md:text-4xl dm-sans font-bold px-5 text-center pb-17">Page Under Construction. Sorry For Your Inconvenience.</h2>
+                    <h2 className="text-[#4F4F4F] text-2xl md:text-4xl dm-sans font-bold px-5 text-center pb-17">Page Under Construction</h2>
                     <div className="w-[60%] md:w-[20%] flex justify-center">
                         <img className="mb-17" src={construction} alt={'construction worker holding plans'}/>
                     </div>
