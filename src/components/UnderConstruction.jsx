34d31f4cd8a96bd203e781b6ab43b8e946c9cf5b
rubenaguilardev <ragui56@wgu.edu>
2025-08-12 17:43:27 -0700

    under construction improvements

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -8,8 +8,10 @@ const UnderConstruction = () => {
             <div className="w-full min-h-screen flex flex-col">
                 <Header />
                 <section className="flex flex-1 flex-col items-center pt-11 md:pt-0 px-3 md:justify-center bg-[#F5F3EA]">
-                    <h2 className="text-[#006B44] text-3xl md:text-4xl font-bold px-20 text-center pb-17">Page Under Construction. Sorry For Your Inconvenience.</h2>
-                    <img className="w-[60%] md:w-full mb-17" src={construction} alt={'construction worker holding plans'}/>
+                    <h2 className="text-[#4F4F4F] text-2xl md:text-4xl dm-sans font-bold px-5 text-center pb-17">Page Under Construction. Sorry For Your Inconvenience.</h2>
+                    <div className="w-[60%] flex justify-center">
+                        <img className="mb-17" src={construction} alt={'construction worker holding plans'}/>
+                    </div>
                     <button
                         type="button"
                         className="bg-white text-[#4F4F4F] text-[1.125rem] font-bold rounded-[40px] border border-[#CAC4D0] px-15 py-6 cursor-pointer shadow-md/60 hover:bg-[#A8D5BA]"
